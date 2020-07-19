593b0256392e6601d87f13c23476f98e3653bb84
yszsz <lm_vivian@foxmail.com>
2020-07-19 09:31:01 +0800

    test: lint

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,29 +1,8 @@
 import React from 'react';
-import { Row, Col, Card } from 'antd';
 
 class Summary extends React.Component {
   render() {
-    return (
-      <div className="site-card-wrapper">
-        <Row gutter={16}>
-          <Col span={8}>
-            <Card title="Card title" bordered={false}>
-              Card content
-            </Card>
-          </Col>
-          <Col span={8}>
-            <Card title="Card title" bordered={false}>
-              Card content
-            </Card>
-          </Col>
-          <Col span={8}>
-            <Card title="Card title" bordered={false}>
-              Card content
-            </Card>
-          </Col>
-        </Row>
-      </div>
-    );
+    return <div className="site-card-wrapper">summary</div>;
   }
 }
 
